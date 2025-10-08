bc75179f16d4b3dc6b45498e5c85a3b17dfb608a
almanetuk-lab <almanetuk@gmail.com>
2025-10-08 22:32:54 +0530

    url issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./components/context/Authprovider.jsx";
+import { AuthProvider } from "./components/context/AuthProvider.jsx";
 import "./index.css";
 import { UserProfileProvider } from "./components/context/UseProfileContext.jsx";
 
