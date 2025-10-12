75b1d23194c9d6f4d640ea9891d2c09d0b7d7bf9
almanetuk-lab <almanetuk@gmail.com>
2025-10-12 22:13:12 +0530

    path issue resolve

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../context/Authprovider";
+import { AuthProvider } from "../context/Authprovider";
 
 export default function Login() {
   const { login } = useAuth();
